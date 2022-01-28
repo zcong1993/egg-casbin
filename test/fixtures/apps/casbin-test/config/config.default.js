d9c2c6eb273299cb3b9d0a96297fa8481ed8b1f7
AI&I <iluozan@126.com>
2022-01-28 15:41:45 +0800

    feat(test): add 403 test case

--- test/fixtures/apps/casbin-test/config/config.default.js
+++ test/fixtures/apps/casbin-test/config/config.default.js
@@ -1,3 +1,26 @@
 'use strict';
 
+const {
+  newEnforcer,
+} = require('casbin');
+const {
+  DefaultAuthorizer,
+} = require('../../../../../lib/index');
+
 exports.keys = '123456';
+
+class MyAuthorizer extends DefaultAuthorizer {
+  // override function
+  getUserName(/* ctx*/) {
+    return 'group';
+  }
+}
+
+exports.casbin = {
+  enable: true,
+  newEnforcer: async () => {
+    const enforcer = await newEnforcer('examples/authz_model.conf', 'examples/authz_policy.csv');
+    return enforcer;
+  },
+  authorizer: MyAuthorizer,
+};
